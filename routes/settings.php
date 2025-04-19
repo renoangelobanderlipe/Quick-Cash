a958c2c905146158940999b5b7c57281ca661156
Reno Angelo Banderlipe <92479031+renoangelobanderlipe@users.noreply.github.com>
2025-04-19 16:29:19 +0800

    refactor: project cleanup (#2)
    * refactor: project cleanup
    
    * fix: resolve lint error
    
    * fix: adjusted workflows
    
    * fix: adjusted workflows
    
    * fix: resolve missing imports

--- routes/settings.php
+++ routes/settings.php
@@ -8,12 +8,15 @@ use Inertia\Inertia;
 Route::middleware('auth')->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('settings/password', [PasswordController::class, 'edit'])->name('password.edit');
-    Route::put('settings/password', [PasswordController::class, 'update'])->name('password.update');
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('settings/profile', 'edit')->name('profile.edit');
+        Route::patch('settings/profile', 'update')->name('profile.update');
+        Route::delete('settings/profile', 'destroy')->name('profile.destroy');
+    });
+    Route::controller(PasswordController::class)->group(function () {
+        Route::get('settings/password',  'edit')->name('password.edit');
+        Route::put('settings/password',  'update')->name('password.update');
+    });
 
     Route::get('settings/appearance', function () {
         return Inertia::render('settings/appearance');
