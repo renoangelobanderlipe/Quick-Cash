7c521736839e11e07d0d0f2e0d5db0dce074c9e1
Reno Angelo D. Banderlipe <personal.renoangelo@gmail.com>
2025-04-19 16:03:46 +0800

    refactor: project cleanup

--- resources/js/pages/auth/forgot-password.tsx
+++ resources/js/pages/auth/forgot-password.tsx
@@ -8,7 +8,7 @@ import TextLink from '@/components/text-link';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import AuthLayout from '@/layouts/auth-layout';
+import AuthSplitLayout from '@/layouts/auth/auth-split-layout';
 
 export default function ForgotPassword({ status }: { status?: string }) {
     const { data, setData, post, processing, errors } = useForm<Required<{ email: string }>>({
@@ -22,7 +22,7 @@ export default function ForgotPassword({ status }: { status?: string }) {
     };
 
     return (
-        <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
+        <AuthSplitLayout title="Forgot password" description="Enter your email to receive a password reset link">
             <Head title="Forgot password" />
 
             {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
@@ -58,6 +58,6 @@ export default function ForgotPassword({ status }: { status?: string }) {
                     <TextLink href={route('login')}>log in</TextLink>
                 </div>
             </div>
-        </AuthLayout>
+        </AuthSplitLayout>
     );
 }
